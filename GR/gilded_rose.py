97eaf040875e8fa7ed5c4d3361b2c1ebf84653be
Laurent Andriamifidy <lolrenx@gmail.com>
2024-05-10 10:21:23 +0000

    implement ABC solution

--- GR/gilded_rose.py
+++ GR/gilded_rose.py
@@ -1,6 +1,6 @@
 # now implement "Wine festival ticket"
 # "Wine festival ticket" degrade in Value twice as fast as normal tickets
-
+from abc import ABC, abstractmethod
 class Ticket:
     def __init__(self, name, concert_countdown, value):
         self.name = name
@@ -10,32 +10,101 @@ class Ticket:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.concert_countdown, self.value)
 
-def update_value(items: list[Ticket]):
-    for item in items:
-        if item.name != "Guns and Roses" and item.name != "Techno festival + backstage + VIP":
-            if item.value > 0:
-                if item.name != "Metallica 2003 collector ticket":
-                    item.value = item.value - 1
+GUNS = "Guns and Roses"
+METALLICA = "Metallica 2003 collector ticket"
+TECHNO = "Techno festival + backstage + VIP"
+WINE_FESTIVAL = "Wine festival ticket"
+
+
+class TicketProcessor(ABC):
+    ticket: Ticket
+
+    @abstractmethod
+    def update_concert_countdown(self):
+        pass
+
+    @abstractmethod
+    def update_value(self):
+        pass
+
+
+class DefaultProcessor(TicketProcessor):
+    def __init__(self, ticket: Ticket):
+        self.ticket = ticket
+
+    def update_concert_countdown(self):
+        self.ticket.concert_countdown = max(0, self.ticket.concert_countdown - 1)
+
+    def update_value(self):
+        if self.ticket.concert_countdown <= 0:
+            self.ticket.value = max(0, self.ticket.value - 2)
         else:
-            if item.value < 50:
-                item.value = item.value + 1
-                if item.name == "Techno festival + backstage + VIP":
-                    if item.concert_countdown < 11:
-                        if item.value < 50:
-                            item.value = item.value + 1
-                    if item.concert_countdown < 6:
-                        if item.value < 50:
-                            item.value = item.value + 1
-        if item.name != "Metallica 2003 collector ticket":
-            item.concert_countdown = item.concert_countdown - 1
-        if item.concert_countdown < 0:
-            if item.name != "Guns and Roses":
-                if item.name != "Techno festival + backstage + VIP":
-                    if item.value > 0:
-                        if item.name != "Metallica 2003 collector ticket":
-                            item.value = item.value - 1
-                else:
-                    item.value = item.value - item.value
-            else:
-                if item.value < 50:
-                    item.value = item.value + 1
+            self.ticket.value = max(0, self.ticket.value - 1)
+
+
+class MetallicaProcessor(TicketProcessor):
+    def __init__(self, ticket: Ticket):
+        self.ticket = ticket
+
+    def update_concert_countdown(self):
+        pass
+
+    def update_value(self):
+        pass
+
+
+class GunsProcessor(DefaultProcessor):
+    def __init__(self, ticket: Ticket):
+        self.ticket = ticket
+
+    def update_concert_countdown(self):
+        super().update_concert_countdown()
+
+    def update_value(self):
+        self.ticket.value = min(50, self.ticket.value + 2)
+
+
+class TechnoProcessor(TicketProcessor):
+    def __init__(self, ticket: Ticket):
+        self.ticket = ticket
+
+    def update_concert_countdown(self):
+        pass
+
+    def update_value(self):
+        if self.ticket.concert_countdown <= 0:
+            self.ticket.value = 0
+        elif self.ticket.concert_countdown <= 5:
+            self.ticket.value = min(50, self.ticket.value + 3)
+        elif self.ticket.concert_countdown <= 10:
+            self.ticket.value = min(50, self.ticket.value + 2)
+        else:
+            self.ticket.value = min(50, self.ticket.value + 1)
+
+
+class WineFestivalProcessor(TicketProcessor):
+    def __init__(self, ticket: Ticket):
+        self.ticket = ticket
+
+    def update_concert_countdown(self):
+        super().update_concert_countdown()
+
+    def update_value(self):
+        if self.ticket.concert_countdown <= 0:
+            self.ticket.value = max(0, self.ticket.value - 4)
+        else:
+            self.ticket.value = max(0, self.ticket.value - 2)
+
+
+def update_value(items: list[Ticket]) -> None:
+    updater = {
+        GUNS: GunsProcessor,
+        METALLICA: MetallicaProcessor,
+        TECHNO: TechnoProcessor, 
+        WINE_FESTIVAL: WineFestivalProcessor
+    }
+
+    for item in items:
+        processor = updater.get(item.name, DefaultProcessor)
+        processor(item).update_concert_countdown()
+        processor(item).update_value()
--- tests/test_wine_festival.py
+++ tests/test_wine_festival.py
@@ -6,7 +6,7 @@ from GR.gilded_rose import (
 WINE_FESTIVAL = "Wine festival ticket"
 
 
-# def test_conjured_degrades_twice_as_fast():
-#     ticket = Ticket(WINE_FESTIVAL, 0, 10)
-#     update_value([ticket])
-#     assert 6 == ticket.value
+def test_conjured_degrades_twice_as_fast():
+    ticket = Ticket(WINE_FESTIVAL, 0, 10)
+    update_value([ticket])
+    assert 6 == ticket.value
